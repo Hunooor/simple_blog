8f3034ff9133ead56a2055c97010a14bef96cc99
Huni <hunooor01@gmail.com>
2024-02-02 12:38:09 +0200

    connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    
+    MongooseModule.forRoot("mongodb://18.197.107.113:27017/blogdb")
   ],
   controllers: [AppController],
   providers: [AppService],
